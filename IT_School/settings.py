95344e45ddd0a8d3671b01d01b669dc04f5f352b
Ilya-Azya <j.stur2502@gmail.com>
2023-03-18 20:37:38 +0100

    3

--- IT_School/settings.py
+++ IT_School/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = '8jxpk8r9*zt44(6g++#&)snipt#u*g!zsh@q)od_2krc8xn=aa'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.176.182.93', 'www.193.176.182.93']
 
 # Application definition
 
@@ -74,12 +74,9 @@ WSGI_APPLICATION = 'IT_School.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'baza22703_113',
-        'USER': 'admin22703_113',
-        'PASSWORD': 's0O!o9I%u7',
-        'HOST': '22703.m.tld.pl',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+
     }
 }
 
