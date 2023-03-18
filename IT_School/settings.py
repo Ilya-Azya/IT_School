a3888ed480bbd268daf1d5586cc3f0cc07c0952e
Ilya-Azya <j.stur2502@gmail.com>
2023-03-18 20:44:16 +0100

    5

--- IT_School/settings.py
+++ IT_School/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/itschool/data/www/IT_School/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
